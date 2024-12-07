5c642816f54151c05b41418fea55dcf16069bdd7
MBrnx <167181692+MBrnx@users.noreply.github.com>
2024-12-07 15:21:43 +0100

    Update platforme.py

--- platforme.py
+++ platforme.py
@@ -4,7 +4,7 @@ class Platforme(pygame.sprite.Sprite):
     def __init__(self, rect):
         super().__init__()
         self.rect = rect
-        self.image = pygame.image.load('image/platforme.png').subsurface((1, 322, 490, 89))
+        self.image = pygame.image.load("Desktop/Projet/images/platforme.png").subsurface((1, 322, 490, 89))
         self.image = pygame.transform.scale(self.image, (self.rect.width, self.rect.height))  # Ajuster la taille de l'image
 
     def afficher(self, surface):
